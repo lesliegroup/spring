8ea5eb3903fe26bbf5f5a43293376e4b1d27a00d
itleslie <itleslie@aliyun.com>
2018-12-23 14:21:46 +0800

    spring_task demo

---
+++ spring-task/src/main/java/com/itleslie/task/SpringTaskApplication.java
@@ -0,0 +1,12 @@
+package com.itleslie.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskApplication.class, args);
+	}
+}
